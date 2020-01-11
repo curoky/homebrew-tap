a97c7bdd673403b2c1323a81158706287711a81d
curoky <cccuroky@gmail.com>
2020-01-11 10:58:13 +0800

    [formula]: add bazel

---
+++ Formula/bazel.rb
@@ -0,0 +1,21 @@
+class Bazel < Formula
+  desc "Google's own build tool"
+  homepage 'https://bazel.build/'
+  license 'Apache-2.0'
+  version '4.1.0'
+  url "https://github.com/bazelbuild/bazel/releases/download/#{version}/bazel-#{version}-installer-linux-x86_64.sh"
+
+  depends_on :linux
+  depends_on 'unzip' # need by installer.sh
+
+  def install
+    system 'bash', "./bazel-#{version}-installer-linux-x86_64.sh", "--prefix=#{prefix}"
+    bash_completion.install "#{prefix}/lib/bazel/bin/bazel-complete.bash"
+    zsh_completion.install "#{prefix}/lib/bazel/bin/_bazel"
+  end
+
+  test do
+    output = shell_output("#{bin}/bazel --version")
+    assert_match "bazel #{version}", output.strip
+  end
+end
