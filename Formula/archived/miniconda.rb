ddb17de7e5b38849de60c5fd92587e8ec5fb6472
curoky <cccuroky@gmail.com>
2020-01-11 10:28:13 +0800

    [formula]: add miniconda

---
+++ Formula/archived/miniconda.rb
@@ -0,0 +1,20 @@
+class Miniconda < Formula
+  desc 'Package, dependency and environment management for any language---Python, R, Ruby, Lua, Scala, Java, JavaScript, C/ C++, FORTRAN' # rubocop:disable Layout/LineLength
+  homepage 'https://docs.conda.io/en/latest/miniconda.html'
+  url 'https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh'
+  version 'head'
+
+  depends_on :linux
+
+  keg_only :versioned_formula
+
+  def install
+    system 'bash', './Miniconda3-latest-Linux-x86_64.sh', '-bfp', prefix.to_s
+    # system "conda", "clean", "-tipsy"
+  end
+
+  test do
+    output = shell_output("#{bin}/conda -V")
+    assert_match 'conda 4.9.2', output.strip
+  end
+end
