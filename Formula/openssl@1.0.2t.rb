440be4cc538c2b4dcefc0aa8bb0d80e34289fde1
curoky <cccuroky@gmail.com>
2020-01-12 22:36:17 +0800

    [openssl@1.0.2t]: add new formula

--- Formula/openssl@1.0.2t.rb
+++ Formula/openssl@1.0.2t.rb
@@ -1,46 +1,82 @@
 # This formula tracks 1.0.2 branch of OpenSSL, not the 1.1.0 branch. Due to
 # significant breaking API changes in 1.1.0 other formulae will be migrated
 # across slowly, so core will ship `openssl` & `openssl@1.1` for foreseeable.
-class OpensslAT102u < Formula
+class OpensslAT102t < Formula
   desc "SSL/TLS cryptography library"
   homepage "https://openssl.org/"
-  url "https://github.com/openssl/openssl/archive/OpenSSL_1_0_2u.tar.gz"
-  sha256 "82fa58e3f273c53128c6fe7e3635ec8cda1319a10ce1ad50a987c3df0deeef05"
+  url "https://www.openssl.org/source/openssl-1.0.2t.tar.gz"
+  sha256 "14cb464efe7ac6b54799b34456bd69558a749a4931ecfd9cf9f71d7881cac7bc"
 
   keg_only :versioned_formula
 
   bottle do
     root_url "https://github.com/curoky/homebrew-tap/releases/download/bottles"
-    sha256 x86_64_linux: "8bfa92d7265a723a961008d664cc781b187520ef55ede0e47628da88e7e55ae7"
+    sha256 x86_64_linux: "8861fd682d47319fe80dd16d006ea2dda71f4372ea924fecb4bec7df302d017b"
   end
 
-  depends_on :linux
-  depends_on "gcc@10"
+  depends_on "make" => :build
+  depends_on "gcc" => :build
 
   def install
-    ENV["CC"] = Formula["gcc"].opt_bin/"gcc-10"
-    ENV["CXX"] = Formula["gcc"].opt_bin/"g++-10"
+    on_linux do
+      ENV["CC"] = Formula["gcc"].opt_bin/"gcc-10"
+      ENV["CXX"] = Formula["gcc"].opt_bin/"g++-10"
+    end
+    
     # OpenSSL will prefer the PERL environment variable if set over $PATH
     # which can cause some odd edge cases & isn't intended. Unset for safety,
     # along with perl modules in PERL5LIB.
     ENV.delete("PERL")
     ENV.delete("PERL5LIB")
-    ENV.append "LDFLAGS", "-L#{buildpath}"
 
     ENV.deparallelize
     args = %W[
       --prefix=#{prefix}
       --openssldir=#{openssldir}
-      shared zlib
+      no-ssl2
+      no-ssl3
+      no-zlib
+      shared
+      enable-cms
     ]
+    args << "linux-x86_64" if OS.linux?
+    args << "darwin64-x86_64-cc" if OS.mac?
+    args << "enable-ec_nistp_64_gcc_128"
 
-    system "./config", *args
-    system "make", "-j#{ENV.make_jobs}"
+    system "perl", "./Configure", *args
+    system "make", "depend"
+    system "make"
+    system "make", "test"
     system "make", "install", "MANDIR=#{man}", "MANSUFFIX=ssl"
   end
 
   def openssldir
     etc/"openssl"
+  end
+
+  def post_install
+    if OS.mac?
+      keychains = %w[
+        /System/Library/Keychains/SystemRootCertificates.keychain
+      ]
+
+      certs_list = `security find-certificate -a -p #{keychains.join(" ")}`
+      certs = certs_list.scan(
+        /-----BEGIN CERTIFICATE-----.*?-----END CERTIFICATE-----/m,
+      )
+
+      valid_certs = certs.select do |cert|
+        IO.popen("#{bin}/openssl x509 -inform pem -checkend 0 -noout", "w") do |openssl_io|
+          openssl_io.write(cert)
+          openssl_io.close_write
+        end
+
+        $CHILD_STATUS.success?
+      end
+
+      openssldir.mkpath
+      (openssldir/"cert.pem").atomic_write(valid_certs.join("\n") << "\n")
+    end
   end
 
   def caveats; <<~EOS
