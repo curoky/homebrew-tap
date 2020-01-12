5be53d1fc9bbd770139acb0a996bc6e4ac0c8cbe
curoky <cccuroky@gmail.com>
2020-01-12 21:54:17 +0800

    [openssl@1.0.2u]: add new formula

---
+++ Formula/openssl@1.0.2u.rb
@@ -0,0 +1,72 @@
+# This formula tracks 1.0.2 branch of OpenSSL, not the 1.1.0 branch. Due to
+# significant breaking API changes in 1.1.0 other formulae will be migrated
+# across slowly, so core will ship `openssl` & `openssl@1.1` for foreseeable.
+class OpensslAT102u < Formula
+  desc 'SSL/TLS cryptography library'
+  homepage 'https://openssl.org/'
+  url 'https://github.com/openssl/openssl/archive/OpenSSL_1_0_2u.tar.gz'
+  sha256 '82fa58e3f273c53128c6fe7e3635ec8cda1319a10ce1ad50a987c3df0deeef05'
+
+  keg_only :versioned_formula
+
+  bottle do
+    root_url 'https://github.com/curoky/homebrew-tap/releases/download/bottles'
+    sha256 x86_64_linux: '0f11cf2ee26100d5b8e685f039895807bfbcd130e36cf7b9a6cfd1d72853f6e1'
+  end
+
+  depends_on :linux
+  depends_on 'gcc@11' => :build
+  depends_on 'xz'
+
+  def install
+    ENV['CC'] = Formula['gcc@11'].opt_bin / 'gcc-11'
+    ENV['CXX'] = Formula['gcc@11'].opt_bin / 'g++-11'
+    # OpenSSL will prefer the PERL environment variable if set over $PATH
+    # which can cause some odd edge cases & isn't intended. Unset for safety,
+    # along with perl modules in PERL5LIB.
+    ENV.delete('PERL')
+    ENV.delete('PERL5LIB')
+    ENV.append 'LDFLAGS', "-L#{buildpath}"
+
+    ENV.deparallelize
+    args = %W[
+      --prefix=#{prefix}
+      --openssldir=#{openssldir}
+      shared zlib
+    ]
+
+    system './config', *args
+    system 'make'
+    system 'make', 'install', "MANDIR=#{man}", 'MANSUFFIX=ssl'
+  end
+
+  def openssldir
+    etc / 'openssl'
+  end
+
+  def caveats
+    <<~EOS
+      A CA file has been bootstrapped using certificates from the SystemRoots
+      keychain. To add additional certificates (e.g. the certificates added in
+      the System keychain), place .pem files in
+        #{openssldir}/certs
+      and run
+        #{opt_bin}/c_rehash
+    EOS
+  end
+
+  test do
+    # Make sure the necessary .cnf file exists, otherwise OpenSSL gets moody.
+    assert_predicate HOMEBREW_PREFIX / 'etc/openssl/openssl.cnf', :exist?,
+                     'OpenSSL requires the .cnf file for some functionality'
+
+    # Check OpenSSL itself functions as expected.
+    (testpath / 'testfile.txt').write('This is a test file')
+    expected_checksum = 'e2d0fe1585a63ec6009c8016ff8dda8b17719a637405a4e23c0ff81339148249'
+    system "#{bin}/openssl", 'dgst', '-sha256', '-out', 'checksum.txt', 'testfile.txt'
+    open('checksum.txt') do |f|
+      checksum = f.read(100).split('=').last.strip
+      assert_equal checksum, expected_checksum
+    end
+  end
+end
