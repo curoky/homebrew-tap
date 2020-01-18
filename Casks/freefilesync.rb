d5315a6124c8b65359a003b13b2ecc6e830c72ab
curoky <cccuroky@gmail.com>
2020-01-18 09:53:59 +0800

    [freefilesync]: add new cask

---
+++ Casks/freefilesync.rb
@@ -0,0 +1,15 @@
+cask 'freefilesync' do
+  version '11.2'
+  sha256 '3f0025823bb5f414c2ed614668eeaf38beec2fc7b6ecb47df5be239de5195135'
+
+  url "https://github.com/curoky/FreeFileSync-Archive/raw/master/FreeFileSync_#{version}_macOS.pkg"
+  name 'FreeFileSync'
+  homepage 'https://www.freefilesync.org/'
+
+  pkg "FreeFileSync_#{version}_macOS.pkg"
+
+  uninstall pkgutil: [
+                       'org.freefilesync.pkg.FreeFileSync',
+                       'org.freefilesync.pkg.RealTimeSync',
+                     ]
+end
