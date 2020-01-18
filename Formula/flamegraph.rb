df3e023c4169a3c302b6dc238632aab3ed3c1b80
curoky <cccuroky@gmail.com>
2020-01-18 10:22:59 +0800

    [flamegraph]: add new formula

---
+++ Formula/flamegraph.rb
@@ -0,0 +1,18 @@
+class Flamegraph < Formula
+  desc 'Stack trace visualizer'
+  homepage 'https://github.com/brendangregg/FlameGraph'
+  url 'https://github.com/brendangregg/FlameGraph/archive/master.zip'
+  license 'CDDL-1.0'
+  head 'https://github.com/brendangregg/FlameGraph.git'
+  version '2.0'
+
+  uses_from_macos 'perl'
+
+  def install
+    bin.install Dir['*.pl']
+    bin.install Dir['*.awk']
+  end
+
+  test do
+  end
+end
