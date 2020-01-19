e57f64835c6791b2dc68a93a6c3610d369de063b
curoky <cccuroky@gmail.com>
2020-01-19 21:45:01 +0800

    [bazel@3]: only support linux

--- Formula/bazel.rb
+++ Formula/bazel.rb
@@ -4,22 +4,14 @@ class Bazel < Formula
   license "Apache-2.0"
   version "3.7.0"
 
-  if OS.mac?
-    url "https://github.com/bazelbuild/bazel/releases/download/3.7.0/bazel-3.7.0-installer-darwin-x86_64.sh"
-    sha256 "436d32ffed7dec442060919be50e1b2076b07302aca55f9ac78cc489eaadc7c9"
-  elsif OS.linux?
-    url "https://github.com/bazelbuild/bazel/releases/download/3.7.0/bazel-3.7.0-installer-linux-x86_64.sh"
-    sha256 "621089dc4d396612603a55f18f55acded29d9b21534ebaa99406a0b4b05029fb"
-  end
+  url "https://github.com/bazelbuild/bazel/releases/download/3.7.0/bazel-3.7.0-installer-linux-x86_64.sh"
+  sha256 "621089dc4d396612603a55f18f55acded29d9b21534ebaa99406a0b4b05029fb"
 
-  # need by installer.sh
-  depends_on "unzip"
-
-  keg_only :versioned_formula
+  depends_on :linux
+  depends_on "unzip" # need by installer.sh
 
   def install
-    fiename = OS.mac? ? "bazel-3.7.0-installer-darwin-x86_64.sh" : "bazel-3.7.0-installer-linux-x86_64.sh"
-    system "bash", "./"+fiename, "--prefix=#{prefix}"
+    system "bash", "./bazel-3.7.0-installer-linux-x86_64.sh", "--prefix=#{prefix}"
     bash_completion.install "#{prefix}/lib/bazel/bin/bazel-complete.bash"
     zsh_completion.install "#{prefix}/lib/bazel/bin/_bazel"
   end
