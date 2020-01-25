ab9ded13cf92d56e8ec25a4c53e2569723b52a6e
curoky <cccuroky@gmail.com>
2020-01-25 10:46:35 +0800

    [bazel]: upgrade to 3.7.2

--- Formula/bazel.rb
+++ Formula/bazel.rb
@@ -2,22 +2,20 @@ class Bazel < Formula
   desc "Google's own build tool"
   homepage "https://bazel.build/"
   license "Apache-2.0"
-  version "3.7.0"
-
-  url "https://github.com/bazelbuild/bazel/releases/download/3.7.0/bazel-3.7.0-installer-linux-x86_64.sh"
-  sha256 "621089dc4d396612603a55f18f55acded29d9b21534ebaa99406a0b4b05029fb"
+  version "4.0.0"
+  url "https://github.com/bazelbuild/bazel/releases/download/#{version}/bazel-#{version}-installer-linux-x86_64.sh"
 
   depends_on :linux
   depends_on "unzip" # need by installer.sh
 
   def install
-    system "bash", "./bazel-3.7.0-installer-linux-x86_64.sh", "--prefix=#{prefix}"
+    system "bash", "./bazel-#{version}-installer-linux-x86_64.sh", "--prefix=#{prefix}"
     bash_completion.install "#{prefix}/lib/bazel/bin/bazel-complete.bash"
     zsh_completion.install "#{prefix}/lib/bazel/bin/_bazel"
   end
 
   test do
     output = shell_output("#{bin}/bazel --version")
-    assert_match "bazel 3.7.0", output.strip
+    assert_match "bazel #{version}", output.strip
   end
 end
