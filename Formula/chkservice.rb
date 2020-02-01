9cc7be739dcf286f9c8559919b3607ed8173a031
curoky <cccuroky@gmail.com>
2020-02-01 17:11:43 +0800

    [chkservice]: add new formula

---
+++ Formula/chkservice.rb
@@ -0,0 +1,35 @@
+class Chkservice < Formula
+  desc "Systemd units manager with ncurses, terminal interface"
+  homepage "https://github.com/linuxenko/chkservice"
+  url "https://github.com/linuxenko/chkservice/archive/master.zip"
+  license "GPL-3.0"
+  version "latest"
+
+  depends_on :linux
+  depends_on "gcc" => :build
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "systemd" => :build
+  depends_on "ncurses" => :build
+
+  def install
+    inreplace "src/CMakeLists.txt",
+      "add_executable(chkservice chkservice.cpp)",
+      "add_executable(chkservice chkservice.cpp)\n target_link_directories(chkservice PRIVATE #{Formula["ncurses"].opt_lib} #{Formula["systemd"].opt_lib})"
+
+    args = std_cmake_args + %W[
+      -GNinja
+      --log-level=STATUS
+    ]
+
+    mkdir "build" do
+      system "cmake", *args, ".."
+      system "ninja"
+      system "ninja", "install"
+    end
+  end
+
+  test do
+    ohai "Test complete."
+  end
+end
